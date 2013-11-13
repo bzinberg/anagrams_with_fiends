7bc64662044a4a18129a324ffdc69a4a59670d03
Ben Zinberg <bzinberg@mit.edu>
2013-11-13 13:15:26 -0500

    create project & disable turbolinks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BzinbergJiangtydYczLapentabFinal::Application.config.secret_key_base = 'ebfa1fd766b9eb5ee6c2cccb14d36855dc97aa5ea50e6d6e314540a995d6b40901f5b1a57389119ec40f647230e74113d54592219549cb279776c153ba6e9b47'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BzinbergJiangtydYczLapentabFinal::Application.config.session_store :cookie_store, key: '_bzinberg_jiangtyd_ycz_lapentab_final_session'
