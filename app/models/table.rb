0e27a2037f568dc1e77c115785317cac50f62e74
Ben Zinberg <bzinberg@mit.edu>
2013-11-15 04:21:54 -0500

    implemented User.request_flip!

--- app/models/table.rb
+++ app/models/table.rb
@@ -1,10 +1,21 @@
 class Table < ActiveRecord::Base
-  has_many :fiends, class_name: 'User'
-  has_many :turns
+  has_many :fiends, class_name: 'User', inverse_of: :table
+  has_many :turns, inverse_of: :table
 
   # One greater than the largest turn number of a turn at this
   # table, or 1 if this table has no turns
   def next_turn_number
     (turns.maximum(:turn_number) || 0) + 1
   end
+
+  # Checks whether all fiends have requested a flip at the next turn number. If
+  # so, creates the requested flip.
+  def register_flip_requests
+    n = next_turn_number
+    if fiends.all? {|u| u.flip_request_turn_number == n}
+      flip = Flip.new(turn_number: n)
+      turns.append(flip)
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
-  belongs_to :table
+  belongs_to :table, inverse_of: :fiends
   has_many :turns
+
+  # Have self request a flip at the next turn number, and have self.table check
+  # whether it's time to grant the request (i.e., whether all other users have
+  # also requested a flip at the next turn number).
+  def request_flip!
+    self.flip_request_turn_number = table.next_turn_number
+    # TODO Do we have to save self to db before running this?  Test.
+    # (Apparently we do; also, if table.fiends has already been loaded to
+    # memory, we may have to run table.fiends.reload)
+    save
+    table.register_flip_requests
+  end
 
 end
