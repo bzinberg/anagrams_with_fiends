2fd921c8a18f848e28896d5c93f2125cbc20c3b4
Ben Zinberg <bzinberg@mit.edu>
2013-11-16 20:35:04 -0500

    moved session code to session helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
 module ApplicationHelper
-  def signed_in?
-    !session[:user_id].nil?
-  end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,9 @@
 module SessionsHelper
+  def current_user
+    @current_user ||= User.find(session[:user_id])
+  end
+
+  def signed_in?
+    current_user.nil?
+  end
 end
