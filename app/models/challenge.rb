c9d53cdd5e4e73e6273f428b547f16ae1ec0a8cb
Ben Zinberg <bzinberg@mit.edu>
2013-12-09 01:00:29 -0500

    try again?

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -3,6 +3,8 @@
 class Challenge < ActiveRecord::Base
   belongs_to :challenger, class_name: 'User', foreign_key: 'challenger_id'
   belongs_to :challengee, class_name: 'User', foreign_key: 'challengee_id'
+  validates :challenger, presence: true
+  validates :challengee, presence: true
 
   def pending?
     self.accepted.nil?
